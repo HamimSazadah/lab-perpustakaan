866571c85427fec439a27e7386acb5ea91f77001
Hamim <tugas.hzh@gmail.com>
2019-04-02 20:54:10 +0700

    update katalog page

--- application/views/pages/katalog.php
+++ application/views/pages/katalog.php
@@ -1,1 +1,17 @@
-test
+<div class="row">
+    <div class="col-md-3">
+        <div class="x_panel"> 
+            <button class="btn btn-primary">Booking</button>
+            <button class="btn btn-warning">Detail</button>       
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="x_panel"></div>
+    </div>
+    <div class="col-md-3">
+        <div class="x_panel"></div>
+    </div>
+    <div class="col-md-3">
+        <div class="x_panel"></div>
+    </div>
+</div>
